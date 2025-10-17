b0ef26c217a316da4cd56b125ced06694ceb534b
David Hary <791370+ATECoder@users.noreply.github.com>
2025-10-17 15:59:30 -0700

    - Update packages:   - .NET Test SDK to 4.0.1.   - Microsoft Extensions to 9.0.10.   - XUnit from 3.1.4 to 3.1.5.   - Fluent Assertions from 8.6.0 to 8.7.1.   - Microsoft.Net.Test.SDK from 17.14.1 to 18.0.0.   - BenchmarkDotNet from 0.14.0 to 0.15.4. - Test projects   - Use [TestMethod( DisplayName = "...']   - Change [ClassCleanup( ClassCleanupBehavior.EndOfClass )] to [ClassCleanup]   - Add parallelize to the assembly attributes.   - change Assert...( ..., format, args ); to Assert...(... string.Format( System.Globalization.CultureInfo.CurrentCulture, format, args ) );   - Use Assert.HasCount<T> in place of Assert.AreEquals( count, [T].Length ). - directory.build.props   - update packages.   - point to the current repository.

--- src/tcp/client.ieee488.mstest/GlobalSuppressions.cs
+++ src/tcp/client.ieee488.mstest/GlobalSuppressions.cs
@@ -6,4 +6,4 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage( "Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "namespace", Target = "~N:cc.isr" )]
-[assembly: SuppressMessage( "Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "namespace", Target = "~N:cc.isr.Iot.Tcp.Client.Ieee488.MSTest" )]
+[assembly: SuppressMessage( "Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "namespace", Target = "~N:cc.isr.Iot.Tcp.Client.Ieee488.Tests" )]
